cbc92b1a84e3d20db92171d01511d42d8295a993
shaul <shaul.rosner@gmail.com>
2022-02-24 20:26:14 +0200

    wrote preliminary version of scorer

--- src/scorer.py
+++ src/scorer.py
@@ -1,9 +1,20 @@
+from collections import defaultdict
+
 from models import Problem, Solution, ScoredSolution
 
 
 # noinspection PyUnusedLocal
 def score_solution(problem: Problem, solution: Solution) -> ScoredSolution:
     score = 0
+    contributor_availability = defaultdict(lambda: 0)
+    for i in range(len(solution.projects)):
+        project = solution.projects[i]
+        contributors = solution.contributors[i]
+        start_time = max([contributor_availability[contributor] for contributor in contributors])
+        end_time = start_time + project.days_needed
+        for contributor in contributors:
+            contributor_availability[contributor] = end_time
+        score += min(1, project.score_reward - (end_time - project.best_before_date))
     return ScoredSolution(
         score=score,
         solution=solution,
