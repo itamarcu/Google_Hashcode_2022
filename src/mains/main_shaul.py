6af2b23b2c3a59a6cb1e4535391b47f5844da31c
shaul <shaul.rosner@gmail.com>
2022-02-24 22:24:47 +0200

    updated shaul_main

--- src/mains/main_shaul.py
+++ src/mains/main_shaul.py
@@ -1,6 +1,7 @@
-from solvers import solution_with_ordering
-from src.run_solution import solve_one
+from src.solvers.solution_with_ordering import solve as solution_with_ordering
+# noinspection PyUnresolvedReferences
+from src.run_solution import solve_one, solve_all
 
 if __name__ == '__main__':
-    solve_one(file_name="a_an_example.in.txt", file_comment="swo", solve=solution_with_ordering)
-    # solve_all(file_comment="swo", solve=solution_with_ordering)
+    solve_one(file_name="b_better_start_small.in.txt", file_comment="swo", solve=solution_with_ordering)
+    #solve_all(file_comment="swo", solve=solution_with_ordering)
