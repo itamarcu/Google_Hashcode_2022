7798b3ca8e30db34a0f527f83e7a9d49c9fd2208
itamar <itamarcu@gmail.com>
2022-02-24 20:11:07 +0200

    Added models

--- src/models.py
+++ src/models.py
@@ -1,12 +1,37 @@
 from dataclasses import dataclass
+from typing import List, Dict
+
+Skill = str
+
+
+@dataclass
+class Contributor:
+    name: str
+    """between 1 to 10"""
+    skills: Dict[Skill, int]
+
+
+@dataclass
+class Project:
+    name: str
+    """between 1 to 10^5"""
+    days_needed: int
+    """between 1 to 10^5"""
+    score_reward: int
+    """between 1 to 10^5"""
+    best_before_date: int
+    """between 1 to 100"""
+    roles_needed: Dict[Skill, int]
 
 
 @dataclass
 class Problem:
-    """
-    name of the problem file.
-    """
+    """name of the problem file."""
     name: str
+    """between 1 to 10^5"""
+    contributors: List[Contributor]
+    """between 1 to 10^5"""
+    projects: List[Project]
 
 
 @dataclass
